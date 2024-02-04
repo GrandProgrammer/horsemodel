e068c192e9aa461e6dc8fe7fc21579d293afcb77
Grand_Programmer <87746145+GrandProgrammer@users.noreply.github.com>
2024-02-04 22:06:27 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import plotly.express as px
 import time
 import platform
 import pathlib
-plt = platform.system() 
-if plt == 'Linux': 
-    pathlib.WindowsPath = pathlib.PosixPath
+# plt = platform.system() 
+# if plt == 'Linux': 
+#     pathlib.WindowsPath = pathlib.PosixPath
 
 # TITLE
 st.title("I KNOW EVERYONE CAN FIND IF THE HORSE PIC WILL BE SHOWN !\n IT IS JUST MODEL FOR FUN :> ")
